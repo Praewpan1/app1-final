87dd9e91838df3d17c000f586b9ef9f9738cce68
Praewpan1 <152370482+Praewpan1@users.noreply.github.com>
2025-03-30 23:55:17 +0700

    Update karaokae.js

--- src/karaokae.js
+++ src/karaokae.js
@@ -1,61 +1,58 @@
-import React, { Component  ,useRef, useState } from "react";
+import React, { Component, useRef, useState } from "react";
 
 export default function Karaokae() {
-    const num1 =useRef()
-    const num2 =useRef()
-    const result =useRef()
-    let [select,setSelect] = useState('')
+    const num1 = useRef()
+    const num2 = useRef()
+    const result = useRef()
+    let [select, setSelect] = useState('')
 
     const onClickButtonTotal = () => {
-      if(select==='nohave'){
-        if(num1.current.value ===''){
-            alert('กรุณากรอกจำนวนชั่วโมง')
+        if (select === 'nohave') {
+            if (num1.current.value === '') {
+                alert('กรุณากรอกจำนวนชั่วโมง')
+            }
+            result.current.value = num1.current.value * 120
         }
-        result.current.value= num1.current.value*120
-      }
-      if(select==='have'){
-        if(num2.current.value ===''){
-            alert('กรุณากรอกจำนวนชั่วโมง')
+        if (select === 'have') {
+            if (num2.current.value === '') {
+                alert('กรุณากรอกจำนวนชั่วโมง')
+            }
+            result.current.value = num2.current.value * 80
         }
-        result.current.value= num2.current.value*80
-      }
-
     }
     const onChange = (ev) => {
-        if(ev.target.id === 'nohave'){
-           num2.current.value=''
-           result.current.value=''
+        if (ev.target.id === 'nohave') {
+            num2.current.value = ''
+            result.current.value = ''
             setSelect('nohave')
         }
-        if(ev.target.id === 'have'){
-            num1.current.value=''
-            result.current.value=''
+        if (ev.target.id === 'have') {
+            num1.current.value = ''
+            result.current.value = ''
             setSelect('have')
         }
-        
-        
-        }
-   
-    
+    }
     return (
-        <div style={{marginTop:10,marginLeft:10}}> 
+        <div style={{ marginTop: 10, marginLeft: 10 }}>
             <table width="400" border="2" bordercolor="#000000">
-                <tr><td colspan="2" id="karaokae">ร้านคาราโอเกะ</td></tr>
-                <tr>
-                    <td>
-                        <br />&nbsp;&nbsp;<input type="radio" name="c1" id="nohave" value="ไม่มีบัตร" onChange={onChange} />ไม่มีบัตรนักศึกษา <br />
-                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ชั่วโมงละ 120 บาท จำนวน
-                        <input type="text" size="2" id="txtqty1"  ref={num1}/> ชั่วโมง <br />
-                        &nbsp;&nbsp;<input type="radio" name="c1" id="have" value="มีบัตร" onChange={onChange} />มีบัตรนักศึกษา <br />
-                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ชั่วโมงละ 80 บาท จำนวน
-                        <input type="text" size="2" id="txtqty2" ref={num2} /> ชั่วโมง <br /><br />
-                    </td>
-                </tr>
+                <thead>
+                    <tr><td colSpan="2" id="karaokae">ร้านคาราโอเกะ</td></tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <br />&nbsp;&nbsp;<input type="radio" name="c1" id="nohave" value="ไม่มีบัตร" onChange={onChange} />ไม่มีบัตรนักศึกษา <br />
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ชั่วโมงละ 120 บาท จำนวน
+                            <input type="text" size="2" id="txtqty1" ref={num1} /> ชั่วโมง <br />
+                            &nbsp;&nbsp;<input type="radio" name="c1" id="have" value="มีบัตร" onChange={onChange} />มีบัตรนักศึกษา <br />
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ชั่วโมงละ 80 บาท จำนวน
+                            <input type="text" size="2" id="txtqty2" ref={num2} /> ชั่วโมง <br /><br />
+                        </td>
+                    </tr>
+                </tbody>
             </table><br />
             <button onClick={onClickButtonTotal} >คิดเงิน</button><br />
-            รวมเป็นเงินทั้งสิ้น = <input type="text" id="txtTotal" readOnly  ref={result}/> บาท
+            รวมเป็นเงินทั้งสิ้น = <input type="text" id="txtTotal" readOnly ref={result} /> บาท
         </div>
-
     );
-
 }
